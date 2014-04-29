020cde05715c3dfbcd089e3969a22b09a2db41fc
Jon Glick <jglick@basho.com>
2014-04-29 11:15:25 -0400

    Fixed odering/refresh frequency

--- puppet/modules/oracle_java/manifests/install.pp
+++ puppet/modules/oracle_java/manifests/install.pp
@@ -3,31 +3,32 @@ class oracle_java::install inherits oracle_java {
   	path => ['/usr/local/bin','/usr/bin', '/bin','/usr/local/sbin','/usr/sbin','/sbin']
   }
 
+  file { 'jvm directory':
+    path => "${jvm_dir}",
+    ensure => directory,
+  }
+
   exec { 'java download':
     command => "wget -O ${staging_package} --no-cookies --no-check-certificate --header 'Cookie: ${accept_cookie}' '${download_url}'",
     creates => "${staging_package}",
     path => ['/usr/local/bin','/usr/bin', '/bin','/usr/local/sbin','/usr/sbin','/sbin'],
-  }
-
-  file { 'jvm directory':
-    path => "${jvm_dir}",
-    ensure => directory
-  }
+  } ~>
 
   exec { 'extract java':
     command => "tar -xzf ${staging_package}",
     cwd => "${jvm_dir}",
-    require => [File['jvm directory'], Exec['java download']],
     creates => "${jvm_dir}/${java_version}",
-  }
+    require => [File['jvm directory']],
+    refreshonly => true
+  } ~>
 
   exec { 'fix perms':
   	command => "find ${jvm_dir} -exec chown root:root {} \\;",
-  	subscribe => Exec['extract java']
-  }
+    refreshonly => true
+  } ~>
 
   exec { 'update alternatives':
     command => "update-alternatives --install /usr/bin/java java ${jvm_dir}/${java_version}/bin/java 0",
-    subscribe => Exec['extract java']
+    refreshonly => true
   }
 }
--- puppet/modules/riakbanana/manifests/install.pp
+++ puppet/modules/riakbanana/manifests/install.pp
@@ -9,15 +9,19 @@ class riakbanana::install inherits riakbanana {
 
   exec { 'create bucket-type':
 	command => "riak-admin bucket-type create data '{\"props\":{}}'",
-	notify => Exec['activate bucket-type']
+  onlyif => "riak-admin bucket-type status data", # bucket-type returns reverse error code 
+	notify => Exec['activate bucket-type'],
+  refreshonly => true
   }
 
   exec { 'activate bucket-type':
     command => "riak-admin bucket-type activate data",
-    notify => Exec['configure bucket']
+    notify => Exec['configure bucket'],
+    refreshonly => true
   }
 
   exec { 'configure bucket': 
-    command => "curl -i -H 'content-type: application/json' -X PUT '${riak_url}/types/data/buckets/${index}/props' -d '{\"props\":{\"search_index\":\"${index}\"}}'"
+    command => "curl -i -H 'content-type: application/json' -X PUT '${riak_url}/types/data/buckets/${index}/props' -d '{\"props\":{\"search_index\":\"${index}\"}}'",
+    refreshonly => true
   }
 }
