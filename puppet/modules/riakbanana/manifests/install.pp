325e70f7b400be96b75db9500406ff51d5eb9d9b
Jon Glick <jglick@basho.com>
2014-06-26 03:47:25 -0400

    Working logstash

--- puppet/modules/riakbanana/manifests/install.pp
+++ puppet/modules/riakbanana/manifests/install.pp
@@ -1,27 +1,59 @@
 class riakbanana::install inherits riakbanana {
 
+# logstash's riak output doesn't currently understand bucket types, so we'll
+#    install them directly via bucket properties
+
+  package { 'git':
+    ensure => installed
+  }
+
+  exec { 'install banana':
+    command => "git clone https://github.com/LucidWorks/banana.git",
+    cwd => "${solr_webapp_dir}",
+    creates => "${solr_webapp_dir}/banana",
+    requires => Package['git'],
+    notify => File['banana dashboard'],
+  }
+
+  file { 'banana dashboard':
+    path => "${solr_webapp_dir}/banana/src/app/dashboards/default.json",
+    content => template("riakbanana/dashboard.json.erb"),
+    ensure => present
+  }
+
+  exec { 'install schema':
+    command => "curl -XPUT -i '${riak_url}/search/schema/${index}' -H 'content-type: application/xml' --data-binary @/vagrant/files/logstash_logs.xml",
+    unless => "curl '${riak_url}/search/schema/${index}' -f",
+  }
 
   exec { 'install index':
-    command => "curl -XPUT -i '${riak_url}/search/index/${index}'",
+    command => "curl -XPUT -i '${riak_url}/search/index/${index}' -H 'content-type: application/xml' --data-binary @/vagrant/files/logstash_logs.xml",
     unless => "curl '${riak_url}/search/index/${index}' -f",
-    notify => Exec['create bucket-type']
+    requires => Exec['install schema'],
+    notify => Exec['configure bucket']
+    #notify => Exec['create bucket-type']
   }
 
-  exec { 'create bucket-type':
-	    command => "riak-admin bucket-type create data '{\"props\":{}}'",
-      unless => "riak-admin bucket-type status data", # bucket-type returns reverse error code
-	    notify => Exec['activate bucket-type'],
-      refreshonly => true
-  }
+  #exec { 'create bucket-type':
+	#    command => "riak-admin bucket-type create data '{\"props\":{}}'",
+  #    unless => "riak-admin bucket-type status data", # bucket-type returns reverse error code
+	#    notify => Exec['activate bucket-type'],
+  #    refreshonly => true
+  #}
 
-  exec { 'activate bucket-type':
-    command => "riak-admin bucket-type activate data",
-    notify => Exec['configure bucket'],
-    refreshonly => true
-  }
+  #exec { 'activate bucket-type':
+  #  command => "riak-admin bucket-type activate data",
+  #  notify => Exec['configure bucket'],
+  #  refreshonly => true
+  #}
 
   exec { 'configure bucket':
-    command => "curl -i -H 'content-type: application/json' -X PUT '${riak_url}/types/data/buckets/${index}/props' -d '{\"props\":{\"search_index\":\"${index}\"}}'",
+    command => "curl -i -H 'content-type: application/json' -X PUT '${riak_url}/buckets/${index}/props' -d '{\"props:\":{\"search_index\":\"${index}\"}}'",
     refreshonly => true
   }
+
+  #exec { 'configure bucket-type':
+  #  command => "curl -i -H 'content-type: application/json' -X PUT '${riak_url}/types/data/buckets/${index}/props' -d '{\"props\":{\"search_index\":\"${index}\"}}'",
+  #  refreshonly => true
+  #}
 }
--- puppet/modules/riakbanana/manifests/params.pp
+++ puppet/modules/riakbanana/manifests/params.pp
@@ -1,8 +1,8 @@
 class riakbanana::params {
-	$index = riakbanana
+	$index = logstash_logs
 	$riak_host = localhost
 	$riak_port = 8098
-
+	$solr_webapp_dir = "/usr/lib/riak/lib/yokozuna-2.0.0beta1-68-g0f0fc47/priv/solr/solr-webapp/webapp"
 	$riak_url = "http://${riak_host}:${riak_port}"
 
 }
